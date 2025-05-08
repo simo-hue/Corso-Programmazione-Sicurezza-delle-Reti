610471d1f4d0cd67df46dfd814a2ade8c22c5778
Mattioli Simone <mattioli.simone.10@gmail.com>
2025-05-08 16:15:35 +0200

    esercizio somma UDP

--- Esempi-client-server/clientUDP_inc.c
+++ Esempi-client-server/clientUDP_inc.c
@@ -9,12 +9,14 @@ int main(void) {
     
     socket = createUDPInterface(20000);
      
+    // Chiedo l'input
     printf("Inserisci un numero intero:\n");
-    
     scanf("%d", &request);
     
+    // Invia request tramite UDP al destinatario avente indirizzo IP "127.0.0.1" e porta 35000
     UDPSend(socket, &request, sizeof(request), "127.0.0.1", 35000);
      
+    // Mettiti in attesa dell’arrivo di un messaggio UDP e metti il valore ricevuto in response
     UDPReceive(socket, &response, sizeof(response), hostAddress, &port);
      
     printf("[CLIENT] Ho ricevuto un messaggio da host/porta %s/%d\n", hostAddress, port);
--- Esempi-client-server/serverUDP_inc.c
+++ Esempi-client-server/serverUDP_inc.c
@@ -14,14 +14,18 @@ int main(void) {
     
     printf("[SERVER] Sono in attesa di richieste da qualche client\n");
     
+    // Mettiti in attesa dell’arrivo di un messaggio UDP e metti il valore ricevuto in request
     UDPReceive(socket, &request, sizeof(request), hostAddress, &port);
     
+
     printf("[SERVER] Ho ricevuto un messaggio da host/porta %s/%d\n", hostAddress, port);
     
     printf("[SERVER] Contenuto: %d\n", request);
     
+    // Elaboro la risposta
     response = request + 1;
     
+    // Invia response a tale mittente tramite UDP
     UDPSend(socket, &response, sizeof(response), hostAddress, port);
 }
 
