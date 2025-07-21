f50b63d6639e7a0bdeadcc149af571728d24bd66
Mattioli Simone <mattioli.simone.10@gmail.com>
2025-07-21 18:02:21 +0200

    chatWEBSOCKET funzionante

---
+++ PER ESAME/WEB e WEBSERVICES/CHAT CON WEB-SOCKET/public/chat.js
@@ -0,0 +1,54 @@
+//Client
+
+var name = prompt("What's your name?");
+while(name == "" || name == null) {
+    name = prompt("You have to choose a name. \n What's your name?");
+}
+
+// Query DOM
+var message = document.getElementById('message'),
+   sender = document.getElementById('sender'),
+   btn = document.getElementById('send'),
+   output = document.getElementById('output'),
+   feedback = document.getElementById('feedback');
+
+sender.innerHTML = name;  
+sender.value = name;
+
+// Invio richiesta di connessione al server (Socket.IO v2)
+var socket = io.connect();
+
+// Gestione connessione
+socket.on('connect', function() {
+    console.log('Connesso al server!');
+});
+
+// Listen for events
+btn.addEventListener('click', function(){
+   if (message.value != "") {    
+       console.log('Invio messaggio:', message.value);
+       socket.emit('message', {
+        message: message.value,
+        sender: sender.value,
+       });
+       message.value = "";
+   }
+});
+
+message.onkeydown = function(e){
+   e = e || window.event;
+   socket.emit('typing', {
+      sender: sender.value
+   });
+}
+
+socket.on('UploadChat', function(data){
+   console.log('Messaggio ricevuto:', data);
+   feedback.innerHTML = '';
+   output.innerHTML += '<p><strong>' + data.sender + ': </strong>' + data.message + '</p>';
+   output.scrollTop = output.scrollHeight;
+});
+
+socket.on('typing', function(data) {
+   feedback.innerHTML = '<p><em>' + data.sender + ' is typing a message...</em></p>';
+});
---
+++ PER ESAME/WEB e WEBSERVICES/CHAT CON WEB-SOCKET/server.js
@@ -0,0 +1,44 @@
+//Server
+
+var express = require('express');
+var socket = require('socket.io');
+
+//Chat setup
+var app = express();
+// in questo momento il server è in attesa delle connessioni HTTP sulla porta 4000
+var server = app.listen(4000, function(){
+   console.log('Server in ascolto sulla porta 4000');
+   console.log('Vai su http://localhost:4000');
+});
+
+// Static files
+/*con questa funzione viene specificato a Nodejs che
+una volta ricevuta una connessione deve andare a
+cercare nella cartella public il file html da fornire
+al client
+*/
+app.use(express.static('public'));
+
+// Socket setup & pass server
+/*una volta che la connessione è stata ricevuta qui
+qui viene effettuato l'upgrade ad una connessione
+websocket e il server si mette in attesa degli
+eventi ai quali rispondere
+*/
+
+var io = socket(server);
+io.on('connection', function(webSocket){
+   console.log('Arrivata una connessione HTTP, attendo upgrade a WebSocket');
+   console.log('made webSocket connection', webSocket.id);
+
+   // Ricezione di un messaggio da inoltrare ai client
+   webSocket.on('message', function(data){
+       console.log('Messaggio ricevuto:', data);
+       io.sockets.emit('UploadChat', data);
+   });
+
+   // Ricezione di un messaggio che l'utente sta scrivendo ( aggiornamento da inoltrare ai client )
+   webSocket.on('typing', function(data) {
+      webSocket.broadcast.emit('typing',data);
+   });
+});
